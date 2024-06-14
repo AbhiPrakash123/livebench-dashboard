32b8c83d33b17dc20a98fc914ca40d28a002e294
Abhishek <1289prakash@gmail.com>
2024-06-14 06:06:30 +0530

    feat: added unoptimized to next/images

--- src/components/labListing/labCard.tsx
+++ src/components/labListing/labCard.tsx
@@ -23,6 +23,7 @@ const Logo = ({ logo_url }: any) => {
                     setImageSoruce(`${process.env.NEXT_PUBLIC_BASE_PATH}/tenxerlabs_blue_logo.webp`)
 
                 }}
+                unoptimized={true}
                 className="rounded-md object-cover h-auto w-full"
             />
         </Card>
@@ -51,6 +52,7 @@ export default function Lab(props: any) {
                         }}
                         alt="board setup image"
                         className="rounded-md object-cover h-auto w-full"
+                        unoptimized={true}
                     />
                 </AspectRatio>
                 <Separator orientation="horizontal" className="my-2" />
